a848e38ea92b7908a12dacc51150a0165fa71c55
Jakub Chojna <jakub.chojna@gmail.com>
2019-01-30 23:00:18 +0100

    Updates

---
+++ precache-manifest.53daf6417a8fa5b7a3ec9688c53130a8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "912577f2ccd8575ffc4a",
+    "url": "/jchojna/drum-machine/static/js/runtime~main.912577f2.js"
+  },
+  {
+    "revision": "383f980c2f7ff8825d1e",
+    "url": "/jchojna/drum-machine/static/js/main.383f980c.chunk.js"
+  },
+  {
+    "revision": "d78a20f7712093c3f480",
+    "url": "/jchojna/drum-machine/static/js/1.d78a20f7.chunk.js"
+  },
+  {
+    "revision": "383f980c2f7ff8825d1e",
+    "url": "/jchojna/drum-machine/static/css/main.4983272f.chunk.css"
+  },
+  {
+    "revision": "ccbff82419a5acc76182743ff9bb179f",
+    "url": "/jchojna/drum-machine/index.html"
+  }
+];
